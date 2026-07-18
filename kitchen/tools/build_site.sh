04d68a4f1d536c4c5a97a4f59c16f9fc2cdcb701
g41797 <g41797@gmail.com>
2026-07-18 11:27:33 +0300

    Fix landing page

--- kitchen/tools/build_site.sh
+++ kitchen/tools/build_site.sh
@@ -30,5 +30,8 @@ echo "--- Building MkDocs site ---"
 cd "$KITCHEN_DIR"
 mkdocs build -f mkdocs.yml
 
+echo "--- Replacing Material default favicon with ours ---"
+cp "$(dirname "$KITCHEN_DIR")/docs/assets/favicon.png" "$(dirname "$KITCHEN_DIR")/docs/assets/images/favicon.png"
+
 echo "--- Done ---"
 echo "Output: $(dirname "$KITCHEN_DIR")/docs/"
